ac9a34110483eb0ed53ac122d4cd7ccc55cd1028
yukishilo <noemail2498fh24@gmail.com>
2019-01-25 16:29:38 +0100

    Support mac

--- src/With.swift
+++ src/With.swift
@@ -1,12 +1,13 @@
 import Foundation
 @discardableResult/*👈 Avoids xcode compiler warnings if result is not used*/
 /**
- * EXAMPLE 1:
+ * ## Examples:
  * let rectangle = with(CGRect.init(x: 0, y: 0, width: 100, height: 100)) {
  *    $0 = $0.offsetBy(dx: 20, dy: 20)
  *    $0 = $0.insetBy(dx: 10, dy: 10)
  * }
  * Swift.print(rectangle)// X:30.0, y:30.0, width:80.0, height:80.0
+ *
  * EXAMPLE 2:
  * let color = with(UIColor.init(red: 50, green: 100, blue: 0, alpha: 0.9)) { ( col:inout UIColor) -> Void  in
  *    col = col.withAlphaComponent(0.2)
